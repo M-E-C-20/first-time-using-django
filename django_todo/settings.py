6167210d879ce70e5d70566e52b2849d33608cb3
M-E-C-20 <mcasey8511@gmail.com>
2021-07-02 01:20:48 +0000

    fix allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(^4w*_k$y#ribjqsaaynhtc&s*(x#)8olgmx9&_h5kqn%dg^(9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['git.heroku.com/django-todo-app-mec-20.git']
 
 
 # Application definition
